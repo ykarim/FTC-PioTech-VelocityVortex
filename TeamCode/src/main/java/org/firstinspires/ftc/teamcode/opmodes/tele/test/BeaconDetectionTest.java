f11bde8185d12cec1e3e819f9ee1cce4d64506d1
ykarim <ykarim@pioneeracademy.org>
2017-02-25 23:01:17 -0500

    Can now move with BeaconDetectionTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/test/BeaconDetectionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/test/BeaconDetectionTest.java
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode.opmodes.tele.test;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.robot.Robot;
 import org.firstinspires.ftc.teamcode.robot.RobotConstants;
+import org.firstinspires.ftc.teamcode.robot.RobotMovement;
 import org.firstinspires.ftc.teamcode.sensors.beacon.BeaconStatus;
 import org.firstinspires.ftc.teamcode.utils.Color;
 import org.firstinspires.ftc.teamcode.utils.OpModeUtils;
@@ -17,6 +19,8 @@ import org.opencv.core.Size;
 @TeleOp (name = "Beacon Detection Test", group = "teletest")
 public class BeaconDetectionTest extends LinearVisionOpMode {
 
+    private Robot robot = new Robot();
+    private RobotMovement robotMovement = new RobotMovement(robot);
     private final String TAG = RobotConstants.autoOpTag + "Beacon Detection Test : ";
 
     @Override
@@ -24,6 +28,11 @@ public class BeaconDetectionTest extends LinearVisionOpMode {
         OpModeUtils.waitFor(this, OpModeUtils.getDelay());
         waitForVisionStart();
         initVision();
+
+        robot.initTeleOp(hardwareMap);
+        gamepad1.setJoystickDeadzone(.1f);
+        gamepad2.setJoystickDeadzone(.1f);
+        robotMovement.orient(RobotMovement.Orientation.RIGHT);
 
         BeaconAnalyzer beaconAnalyzer = new BeaconAnalyzer();
         Thread beaconUpdate = new Thread(beaconAnalyzer, "Beacon Analyzer");
@@ -34,6 +43,7 @@ public class BeaconDetectionTest extends LinearVisionOpMode {
         while (opModeIsActive()) {
             updateTelemetry();
 
+            robotMovement.move(convertGamepadToMovement());
             if (gamepad1.a) {
                 while (gamepad1.a) {}
                 beaconAnalyzer.stop();
@@ -63,6 +73,37 @@ public class BeaconDetectionTest extends LinearVisionOpMode {
 
         cameraControl.setColorTemperature(CameraControlExtension.ColorTemperature.AUTO);
         cameraControl.setAutoExposureCompensation();
+    }
+
+    /**
+     * Converts gamepad-1 x and y coords to robot directions
+     * @return Direction
+     */
+    private RobotMovement.Direction convertGamepadToMovement() {
+        if (gamepad1.left_stick_y > RobotConstants.gamepadThreshold &&
+                !inThresholdRange(gamepad1.left_stick_x)) {
+            return RobotMovement.Direction.SOUTH;
+        } else if (gamepad1.left_stick_y < -RobotConstants.gamepadThreshold &&
+                !inThresholdRange(gamepad1.left_stick_x)) {
+            return RobotMovement.Direction.NORTH;
+        } else if (gamepad1.left_stick_x > RobotConstants.gamepadThreshold &&
+                !inThresholdRange(gamepad1.left_stick_y)) {
+            return RobotMovement.Direction.EAST;
+        } else if (gamepad1.left_stick_x < -RobotConstants.gamepadThreshold &&
+                !inThresholdRange(gamepad1.left_stick_y)) {
+            return RobotMovement.Direction.WEST;
+        } else if (inThresholdRange(gamepad1.left_trigger)) {
+            return RobotMovement.Direction.ROTATE_LEFT;
+        } else if (inThresholdRange(gamepad1.right_trigger)) {
+            return RobotMovement.Direction.ROTATE_RIGHT;
+        } else {
+            return RobotMovement.Direction.NONE;
+        }
+    }
+
+    private boolean inThresholdRange(double val) {
+        return (val > RobotConstants.gamepadThreshold ||
+                val < -RobotConstants.gamepadThreshold);
     }
 
     private void updateTelemetry() {
